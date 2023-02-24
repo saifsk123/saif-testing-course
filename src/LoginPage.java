131432db3ef4325484aa9963e9ec0f2160f7d866
saifsk123 <saifkhanarbaaz.sk@gmail.com>
2023-02-24 23:52:05 +0530

    Adding login button code

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -2,6 +2,8 @@ public class LoginPage {
     public void verifyloginbutton()
     {
         String s1="naveen1";
-        String s2="naveen3"
+        String s2="naveen3";
+        String s3="naveen4";
     }
+
 }

2d1c5b5feebca5ac043a4eac7753fbf03b5c36ea
saifsk123 <124482574+saifsk123@users.noreply.github.com>
2023-02-24 23:50:21 +0530

    Update LoginPage.java

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -2,6 +2,7 @@ public class LoginPage {
     public void verifyloginbutton()
     {
         String s1="naveen1";
-        String s2="naveen3"
+        String s2="naveen3";
+        String s4="naveen5";
     }
 }
